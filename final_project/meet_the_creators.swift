9964ee2e841daf130a6a851bd728bfedbeb06e50
liub232 <liubecky232@gmail.com>
2024-06-27 09:57:34 -0700

    add creators page

---
+++ final_project/meet_the_creators.swift
@@ -0,0 +1,18 @@
+//
+//  meet_the_creators.swift
+//  final_project
+//
+//  Created by Scholar on 6/27/24.
+//
+
+import SwiftUI
+
+struct meet_the_creators: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    meet_the_creators()
+}
